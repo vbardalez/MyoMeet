4fcd6b2a13d9a7ebe5ed9b5f61c154ed77186216
Vlad Bardalez <bardalezvlad@gmail.com>
2015-11-15 00:41:07 -0500

    Saturday Night Commit

--- MyoMeet/Source.cpp
+++ MyoMeet/Source.cpp
@@ -1,168 +1,75 @@
 // Copyright (C) 2013-2014 Thalmic Labs Inc.
 // Distributed under the Myo SDK license agreement. See LICENSE.txt for details.
-#define _USE_MATH_DEFINES
-#include <cmath>
+// This sample illustrates how to interface with multiple Myo armbands and distinguish between them.
 #include <iostream>
-#include <iomanip>
 #include <stdexcept>
-#include <string>
-#include <algorithm>
-// The only file that needs to be included to use the Myo C++ SDK is myo.hpp.
+#include <vector>
+#include <windows.h>
 #include <myo/myo.hpp>
-// Classes that inherit from myo::DeviceListener can be used to receive events from Myo devices. DeviceListener
-// provides several virtual functions for handling different kinds of events. If you do not override an event, the
-// default behavior is to do nothing.
-class DataCollector : public myo::DeviceListener {
+class MyoMeeterDL : public myo::DeviceListener {
 public:
-	DataCollector()
-		: onArm(false), isUnlocked(false), roll_w(0), pitch_w(0), yaw_w(0), currentPose()
+	// Every time Myo Connect successfully pairs with a Myo armband, this function will be called.
+	//
+	// You can rely on the following rules:
+	//  - onPair() will only be called once for each Myo device
+	//  - no other events will occur involving a given Myo device before onPair() is called with it
+	//
+	// If you need to do some kind of per-Myo preparation before handling events, you can safely do it in onPair().
+	void onPair(myo::Myo* myo, uint64_t timestamp, myo::FirmwareVersion firmwareVersion)
 	{
+		// Print out the MAC address of the armband we paired with.
+		// The pointer address we get for a Myo is unique - in other words, it's safe to compare two Myo pointers to
+		// see if they're referring to the same Myo.
+		// Add the Myo pointer to our list of known Myo devices. This list is used to implement identifyMyo() below so
+		// that we can give each Myo a nice short identifier.
+		knownMyos.push_back(myo);
+		// Now that we've added it to our list, get our short ID for it and print it out.
+		std::cout << "Paired with " << myo << "." << std::endl;
 	}
-	// onUnpair() is called whenever the Myo is disconnected from Myo Connect by the user.
-	void onUnpair(myo::Myo* myo, uint64_t timestamp)
-	{
-		// We've lost a Myo.
-		// Let's clean up some leftover state.
-		roll_w = 0;
-		pitch_w = 0;
-		yaw_w = 0;
-		onArm = false;
-		isUnlocked = false;
-	}
-	// onOrientationData() is called whenever the Myo device provides its current orientation, which is represented
-	// as a unit quaternion.
-	void onOrientationData(myo::Myo* myo, uint64_t timestamp, const myo::Quaternion<float>& quat)
-	{
-		using std::atan2;
-		using std::asin;
-		using std::sqrt;
-		using std::max;
-		using std::min;
-		// Calculate Euler angles (roll, pitch, and yaw) from the unit quaternion.
-		float roll = atan2(2.0f * (quat.w() * quat.x() + quat.y() * quat.z()),
-			1.0f - 2.0f * (quat.x() * quat.x() + quat.y() * quat.y()));
-		float pitch = asin(max(-1.0f, min(1.0f, 2.0f * (quat.w() * quat.y() - quat.z() * quat.x()))));
-		float yaw = atan2(2.0f * (quat.w() * quat.z() + quat.x() * quat.y()),
-			1.0f - 2.0f * (quat.y() * quat.y() + quat.z() * quat.z()));
-		// Convert the floating point angles in radians to a scale from 0 to 18.
-		roll_w = static_cast<int>((roll + (float)M_PI) / (M_PI * 2.0f) * 18);
-		pitch_w = static_cast<int>((pitch + (float)M_PI / 2.0f) / M_PI * 18);
-		yaw_w = static_cast<int>((yaw + (float)M_PI) / (M_PI * 2.0f) * 18);
-	}
-	// onPose() is called whenever the Myo detects that the person wearing it has changed their pose, for example,
-	// making a fist, or not making a fist anymore.
+
 	void onPose(myo::Myo* myo, uint64_t timestamp, myo::Pose pose)
 	{
-		currentPose = pose;
-		if (pose != myo::Pose::unknown && pose != myo::Pose::rest) {
-			// Tell the Myo to stay unlocked until told otherwise. We do that here so you can hold the poses without the
-			// Myo becoming locked.
-			myo->unlock(myo::Myo::unlockHold);
-			// Notify the Myo that the pose has resulted in an action, in this case changing
-			// the text on the screen. The Myo will vibrate.
-			myo->notifyUserAction();
+		std::cout << "Myo " << identifyMyo(myo) << " switched to pose " << pose.toString() << "." << std::endl;
+		//currentPoses[identifyMyo(myo)] = pose;
+	}
+	void onConnect(myo::Myo* myo, uint64_t timestamp, myo::FirmwareVersion firmwareVersion)
+	{
+		myo->unlock(myo::Myo::unlockHold);
+		std::cout << "Myo " << identifyMyo(myo) << " has connected." << std::endl;
+	}
+	void onDisconnect(myo::Myo* myo, uint64_t timestamp)
+	{
+		std::cout << "Myo " << identifyMyo(myo) << " has disconnected." << std::endl;
+	}
+	// This is a utility function implemented for this sample that maps a myo::Myo* to a unique ID starting at 1.
+	// It does so by looking for the Myo pointer in knownMyos, which onPair() adds each Myo into as it is paired.
+	size_t identifyMyo(myo::Myo* myo) {
+		// Walk through the list of Myo devices that we've seen pairing events for.
+		for (size_t i = 0; i < knownMyos.size(); ++i) {
+			// If two Myo pointers compare equal, they refer to the same Myo device.
+			if (knownMyos[i] == myo) {
+				return i + 1;
+			}
 		}
-		else {
-			// Tell the Myo to stay unlocked only for a short period. This allows the Myo to stay unlocked while poses
-			// are being performed, but lock after inactivity.
-			myo->unlock(myo::Myo::unlockTimed);
-		}
+		return 0;
 	}
-	// onArmSync() is called whenever Myo has recognized a Sync Gesture after someone has put it on their
-	// arm. This lets Myo know which arm it's on and which way it's facing.
-	void onArmSync(myo::Myo* myo, uint64_t timestamp, myo::Arm arm, myo::XDirection xDirection, float rotation,
-		myo::WarmupState warmupState)
-	{
-		onArm = true;
-		whichArm = arm;
-	}
-	// onArmUnsync() is called whenever Myo has detected that it was moved from a stable position on a person's arm after
-	// it recognized the arm. Typically this happens when someone takes Myo off of their arm, but it can also happen
-	// when Myo is moved around on the arm.
-	void onArmUnsync(myo::Myo* myo, uint64_t timestamp)
-	{
-		onArm = false;
-	}
-	// onUnlock() is called whenever Myo has become unlocked, and will start delivering pose events.
-	void onUnlock(myo::Myo* myo, uint64_t timestamp)
-	{
-		isUnlocked = true;
-	}
-	// onLock() is called whenever Myo has become locked. No pose events will be sent until the Myo is unlocked again.
-	void onLock(myo::Myo* myo, uint64_t timestamp)
-	{
-		isUnlocked = false;
-	}
-	// There are other virtual functions in DeviceListener that we could override here, like onAccelerometerData().
-	// For this example, the functions overridden above are sufficient.
-	// We define this function to print the current values that were updated by the on...() functions above.
-	void print()
-	{
-		// Clear the current line
-		std::cout << '\r';
-		// Print out the orientation. Orientation data is always available, even if no arm is currently recognized.
-		std::cout << '[' << std::string(roll_w, '*') << std::string(18 - roll_w, ' ') << ']'
-			<< '[' << std::string(pitch_w, '*') << std::string(18 - pitch_w, ' ') << ']'
-			<< '[' << std::string(yaw_w, '*') << std::string(18 - yaw_w, ' ') << ']';
-		if (onArm) {
-			// Print out the lock state, the currently recognized pose, and which arm Myo is being worn on.
-			// Pose::toString() provides the human-readable name of a pose. We can also output a Pose directly to an
-			// output stream (e.g. std::cout << currentPose;). In this case we want to get the pose name's length so
-			// that we can fill the rest of the field with spaces below, so we obtain it as a string using toString().
-			std::string poseString = currentPose.toString();
-			std::cout << '[' << (isUnlocked ? "unlocked" : "locked  ") << ']'
-				<< '[' << (whichArm == myo::armLeft ? "L" : "R") << ']'
-				<< '[' << poseString << std::string(14 - poseString.size(), ' ') << ']';
-		}
-		else {
-			// Print out a placeholder for the arm and pose when Myo doesn't currently know which arm it's on.
-			std::cout << '[' << std::string(8, ' ') << ']' << "[?]" << '[' << std::string(14, ' ') << ']';
-		}
-		std::cout << std::flush;
-	}
-	// These values are set by onArmSync() and onArmUnsync() above.
-	bool onArm;
-	myo::Arm whichArm;
-	// This is set by onUnlocked() and onLocked() above.
-	bool isUnlocked;
-	// These values are set by onOrientationData() and onPose() above.
-	int roll_w, pitch_w, yaw_w;
-	myo::Pose currentPose;
+	// We store each Myo pointer that we pair with in this list, so that we can keep track of the order we've seen
+	// each Myo and give it a unique short identifier (see onPair() and identifyMyo() above).
+	std::vector<myo::Myo*> knownMyos;
 };
+
+
 int main(int argc, char** argv)
 {
-	// We catch any exceptions that might occur below -- see the catch statement for more details.
 	try {
-		// First, we create a Hub with our application identifier. Be sure not to use the com.example namespace when
-		// publishing your application. The Hub provides access to one or more Myos.
-		myo::Hub hub("com.example.hello-myo");
-		std::cout << "Attempting to find a Myo..." << std::endl;
-		// Next, we attempt to find a Myo to use. If a Myo is already paired in Myo Connect, this will return that Myo
-		// immediately.
-		// waitForMyo() takes a timeout value in milliseconds. In this case we will try to find a Myo for 10 seconds, and
-		// if that fails, the function will return a null pointer.
-		myo::Myo* myo = hub.waitForMyo(10000);
-		// If waitForMyo() returned a null pointer, we failed to find a Myo, so exit with an error message.
-		if (!myo) {
-			throw std::runtime_error("Unable to find a Myo!");
-		}
-		// We've found a Myo.
-		std::cout << "Connected to a Myo armband!" << std::endl << std::endl;
-		// Next we construct an instance of our DeviceListener, so that we can register it with the Hub.
-		DataCollector collector;
-		// Hub::addListener() takes the address of any object whose class inherits from DeviceListener, and will cause
-		// Hub::run() to send events to all registered device listeners.
-		hub.addListener(&collector);
-		// Finally we enter our main loop.
+		myo::Hub hub("com.example.multiple-myos");
+		// Instantiate the PrintMyoEvents class we defined above, and attach it as a listener to our Hub.
+		MyoMeeterDL myoMeeter;
+		hub.addListener(&myoMeeter);
 		while (1) {
-			// In each iteration of our main loop, we run the Myo event loop for a set number of milliseconds.
-			// In this case, we wish to update our display 20 times a second, so we run for 1000/20 milliseconds.
-			hub.run(1000 / 20);
-			// After processing events, we call the print() member function we defined above to print out the values we've
-			// obtained from any events that have occurred.
-			collector.print();
+			// Process events for 10 milliseconds at a time.
+			hub.run(10);
 		}
-		// If a standard exception occurred, we print out its message and exit.
 	}
 	catch (const std::exception& e) {
 		std::cerr << "Error: " << e.what() << std::endl;
